5b5639d3ecc930f1210348f595b5b69057ff7b66
86134 <1591128445@qq.com>
2022-12-24 20:23:53 +0800

    这是二次测试

---
+++ study02/src/Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+        String[] str=new String[10];
+        str[0]="1";
+        str[1]="2";
+        str[2]="3";
+        str[3]="4";
+        str[4]="5";
+        for(String s:str){
+            System.out.println(s);
+        }
+
+    }
+}
